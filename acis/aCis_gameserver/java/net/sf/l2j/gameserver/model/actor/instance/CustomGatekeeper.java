d533324d131ac225bb41567bdfdd80ed115fc2c3
jojikas20 <ma17das@gmail.com>
2018-06-23 19:58:05 +0200

    Custom Gatekeeper 30990
    //spawn 30990

---
+++ acis/aCis_gameserver/java/net/sf/l2j/gameserver/model/actor/instance/CustomGatekeeper.java
@@ -0,0 +1,79 @@
+package net.sf.l2j.gameserver.model.actor.instance;
+
+import net.sf.l2j.Config;
+import net.sf.l2j.gameserver.cache.HtmCache;
+import net.sf.l2j.gameserver.custom.FactionZoneManager;
+import net.sf.l2j.gameserver.datatables.TeleportLocationTable;
+import net.sf.l2j.gameserver.instancemanager.CastleManager;
+import net.sf.l2j.gameserver.instancemanager.ZoneManager;
+import net.sf.l2j.gameserver.model.L2TeleportLocation;
+import net.sf.l2j.gameserver.model.Location;
+import net.sf.l2j.gameserver.model.actor.template.NpcTemplate;
+import net.sf.l2j.gameserver.model.entity.Siege;
+import net.sf.l2j.gameserver.model.zone.L2ZoneType;
+import net.sf.l2j.gameserver.model.zone.type.L2FactionZone;
+import net.sf.l2j.gameserver.model.zone.type.L2TownZone;
+import net.sf.l2j.gameserver.network.SystemMessageId;
+import net.sf.l2j.gameserver.network.serverpackets.ActionFailed;
+import net.sf.l2j.gameserver.network.serverpackets.NpcHtmlMessage;
+
+import java.util.StringTokenizer;
+
+public class CustomGatekeeper extends Folk {
+    public CustomGatekeeper(int objectId, NpcTemplate template)
+    {
+        super(objectId, template);
+    }
+
+    @Override
+    public String getHtmlPath(int npcId, int val)
+    {
+        String filename = "";
+        if (val == 0)
+            filename = "" + npcId;
+        else
+            filename = npcId + "-" + val;
+
+        return "data/html/customTeleporter/" + filename + ".htm";
+    }
+
+    @Override
+    public void onBypassFeedback(Player player, String command)
+    {
+        player.sendPacket(ActionFailed.STATIC_PACKET);
+        if (command.startsWith("goto")) {
+            final StringTokenizer st = new StringTokenizer(command, " ");
+            st.nextToken();
+
+            if (st.countTokens() <= 0)
+                return;
+            L2TeleportLocation loc = TeleportLocationTable.getInstance().getTemplate(Integer.parseInt(st.nextToken()));
+            if (loc == null)
+                return;
+            player.teleToLocation(loc.getLocX(), loc.getLocY(), loc.getLocZ(), 20);
+            player.sendPacket(ActionFailed.STATIC_PACKET);
+        }
+        if(command.startsWith("faction")){
+            Location loc = ZoneManager.getInstance().getZoneById(FactionZoneManager.getInstance().getCurrentFactionZoneId(),L2FactionZone.class).getSpawnLoc();
+            player.teleToLocation(loc.getX(), loc.getY(), loc.getZ(), 20);
+            player.sendPacket(ActionFailed.STATIC_PACKET);
+        }
+
+
+    }
+
+    @Override
+    public void showChatWindow(Player player)
+    {
+        String filename = getHtmlPath(getNpcId(), 0);
+
+        final NpcHtmlMessage html = new NpcHtmlMessage(getObjectId());
+        html.setFile(filename);
+        int factionZoneId = FactionZoneManager.getInstance().getCurrentFactionZoneId();
+        L2ZoneType currentFactionZone = ZoneManager.getInstance().getZoneById(factionZoneId);
+        html.replace("%onlineInGiran%",ZoneManager.getInstance().getZone(83400,148104,-3400,L2TownZone.class).getKnownTypeInside(Player.class).size());
+        html.replace("%onlineInFaction%",currentFactionZone.getKnownTypeInside(Player.class).size());
+        html.replace("%objectId%", getObjectId());
+        player.sendPacket(html);
+    }
+}
--- acis/aCis_gameserver/java/net/sf/l2j/gameserver/model/zone/type/L2FactionZone.java
+++ acis/aCis_gameserver/java/net/sf/l2j/gameserver/model/zone/type/L2FactionZone.java
@@ -5,7 +5,6 @@ import net.sf.l2j.gameserver.custom.FactionZoneManager;
 import net.sf.l2j.gameserver.datatables.MapRegionTable;
 import net.sf.l2j.gameserver.model.Location;
 import net.sf.l2j.gameserver.model.actor.Creature;
-import net.sf.l2j.gameserver.model.actor.instance.Gatekeeper;
 import net.sf.l2j.gameserver.model.actor.instance.Player;
 import net.sf.l2j.gameserver.model.zone.L2SpawnZone;
 import net.sf.l2j.gameserver.model.zone.ZoneId;
@@ -21,35 +20,37 @@ public class L2FactionZone extends L2SpawnZone {
 
     @Override
     protected void onEnter(Creature character) {
-        if(FactionZoneManager.getInstance().getCurrentFactionZoneId() != this.getId()) {
-            Location loc = MapRegionTable.getTown(9).getSpawnLoc();
-            character.teleToLocation(loc.getX(), loc.getY(), loc.getZ(), 100);
-            return;
-        }
-        if (character instanceof Player)
-        {
+        if (character instanceof Player) {
+            if(FactionZoneManager.getInstance().getCurrentFactionZoneId() != this.getId()) {
+                Location loc = MapRegionTable.getTown(9).getSpawnLoc();
+                character.teleToLocation(loc.getX(), loc.getY(), loc.getZ(), 100);
+                return;
+            }
+
             if (!character.isInsideZone(ZoneId.FACTION))
                 ((Player) character).sendPacket(SystemMessageId.ENTERED_COMBAT_ZONE);
+
+            Player player = (Player) character;
+            player.setInsideZone(ZoneId.FACTION, true);
+            FactionHelper.getInstance().onZoneEntrance(player);
+            player.sendMessage("There are: " + getCharactersInside().size() + " players in zone.");
+            player.broadcastTitleInfo();
+            player.broadcastCharInfo();
         }
-        Player player = (Player)character;
-        player.setInsideZone(ZoneId.FACTION,true);
-        FactionHelper.getInstance().onZoneEntrance(player);
-        player.sendMessage("There are: " + getCharactersInside().size() + " players in zone.");
-        player.broadcastTitleInfo();
-        player.broadcastCharInfo();
     }
 
     @Override
     protected void onExit(Creature character) {
-        Player player = (Player) character;
-        player.setInsideZone(ZoneId.FACTION,false);
         if (character instanceof Player) {
             if (!character.isInsideZone(ZoneId.FACTION))
                 ((Player) character).sendPacket(SystemMessageId.LEFT_COMBAT_ZONE);
+
+            Player player = (Player) character;
+            player.setInsideZone(ZoneId.FACTION,false);
+            FactionHelper.getInstance().onZoneExit(player);
+            player.broadcastTitleInfo();
+            player.broadcastCharInfo();
         }
-        FactionHelper.getInstance().onZoneExit(player);
-        player.broadcastTitleInfo();
-        player.broadcastCharInfo();
     }
 
     @Override
